aba4662899880c8d18e02a5b6d020d26676f3759
Ainferno <vasily.nichiporenko@ya.ru>
2022-04-25 18:26:52 +0300

    bubble sort

---
+++ bubblesort.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func bubble_sort(a []int) []int {
+	len_a := len(a)
+	for i := 0; i < len_a; i++ {
+		for j := 0; j < len_a-1; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+			}
+		}
+	}
+
+	return a
+}
+
+func main() {
+	a := []int{1, 2, 7, 5, 6, 3, 4}
+	fmt.Println(bubble_sort(a))
+}
